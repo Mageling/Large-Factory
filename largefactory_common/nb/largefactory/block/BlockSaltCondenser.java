fbc3cbc247d84b4310a85fd4ad48f293acd183c6
Mageling <nksadow@gmail.com>
2013-05-29 14:04:53 -0600

    Added Salt condenser. Has no GUI and no crafting recipe

---
+++ largefactory_common/nb/largefactory/block/BlockSaltCondenser.java
@@ -0,0 +1,86 @@
+package nb.largefactory.block;
+
+import java.util.Random;
+
+import nb.largefactory.tileentity.TileEntitySaltCondenser;
+import net.minecraft.block.BlockContainer;
+import net.minecraft.block.material.Material;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.World;
+
+public class BlockSaltCondenser extends BlockContainer {
+
+    private final Random saltRand = new Random();
+
+    protected BlockSaltCondenser(int par1) {
+        super(par1, Material.wood);
+    }
+
+    @Override
+    public TileEntity createNewTileEntity(World world) {
+        return null;
+    }
+
+    @Override
+    public int idDropped(int par1, Random par2Random, int par3) {
+        return 0;
+    }
+
+    @Override
+    public void breakBlock(World par1World, int par2, int par3, int par4,
+            int par5, int par6) {
+
+        TileEntitySaltCondenser tileentitysaltcondenser = (TileEntitySaltCondenser) par1World
+                .getBlockTileEntity(par2, par3, par4);
+
+        if (tileentitysaltcondenser != null) {
+            for (int j1 = 0; j1 < tileentitysaltcondenser.getSizeInventory(); ++j1) {
+                ItemStack itemstack = tileentitysaltcondenser
+                        .getStackInSlot(j1);
+
+                if (itemstack != null) {
+                    float f = saltRand.nextFloat() * 0.8F + 0.1F;
+                    float f1 = saltRand.nextFloat() * 0.8F + 0.1F;
+                    float f2 = saltRand.nextFloat() * 0.8F + 0.1F;
+
+                    while (itemstack.stackSize > 0) {
+                        int k1 = saltRand.nextInt(21) + 10;
+
+                        if (k1 > itemstack.stackSize) {
+                            k1 = itemstack.stackSize;
+                        }
+
+                        itemstack.stackSize -= k1;
+                        EntityItem entityitem = new EntityItem(par1World, par2
+                                + f, par3 + f1, par4 + f2,
+                                new ItemStack(itemstack.itemID, k1,
+                                        itemstack.getItemDamage()));
+
+                        if (itemstack.hasTagCompound()) {
+                            entityitem.getEntityItem().setTagCompound(
+                                    (NBTTagCompound) itemstack.getTagCompound()
+                                            .copy());
+                        }
+
+                        float f3 = 0.05F;
+                        entityitem.motionX = (float) saltRand.nextGaussian()
+                                * f3;
+                        entityitem.motionY = (float) saltRand.nextGaussian()
+                                * f3 + 0.2F;
+                        entityitem.motionZ = (float) saltRand.nextGaussian()
+                                * f3;
+                        par1World.spawnEntityInWorld(entityitem);
+                    }
+                }
+
+                par1World.func_96440_m(par2, par3, par4, par5);
+            }
+        }
+
+        super.breakBlock(par1World, par2, par3, par4, par5, par6);
+    }
+
+}
---
+++ largefactory_common/nb/largefactory/tileentity/TileEntitySaltCondenser.java
@@ -0,0 +1,211 @@
+package nb.largefactory.tileentity;
+
+import nb.largefactory.item.ItemLargeFactory;
+import net.minecraft.block.Block;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.biome.BiomeGenBase;
+
+public class TileEntitySaltCondenser extends TileEntity implements IInventory {
+
+    private ItemStack[] saltCondenserItemStacks = new ItemStack[1];
+    public int saltCondenserTimeRemaining = 20304000;
+    public int saltProgress = 0;
+    public boolean ceaseFunction = false;
+    private String customName;
+
+    @Override
+    public void updateEntity() {
+        if (saltCondenserTimeRemaining > 0) {
+            saltCondenserTimeRemaining--;
+            if (this.canProduce()) {
+                if (worldObj.getBiomeGenForCoords(xCoord, zCoord) == BiomeGenBase.beach
+                        || worldObj.getBiomeGenForCoords(xCoord, zCoord) == BiomeGenBase.ocean) {
+                    saltProgress = saltProgress + 16;
+                } else {
+                    saltProgress = saltProgress + 1;
+                }
+                if (saltProgress >= 576000) {
+                    this.makeSalt();
+                    saltProgress = saltProgress - 576000;
+                }
+            }
+        } else {
+            ceaseFunction = true;
+        }
+    }
+
+    private void makeSalt() {
+        if (saltCondenserItemStacks[0] == null) {
+            saltCondenserItemStacks[0] = new ItemStack(ItemLargeFactory.salt, 1);
+            return;
+        } else {
+            if (saltCondenserItemStacks[0].itemID == ItemLargeFactory.salt.itemID) {
+                saltCondenserItemStacks[0] = new ItemStack(
+                        ItemLargeFactory.salt,
+                        saltCondenserItemStacks[0].stackSize + 1);
+                return;
+            }
+        }
+    }
+
+    private boolean canProduce() {
+        if (worldObj.canBlockSeeTheSky(xCoord, yCoord, zCoord)) {
+            if (worldObj.getBlockId(xCoord + 1, yCoord, zCoord) == Block.waterStill.blockID)
+                return true;
+            if (worldObj.getBlockId(xCoord - 1, yCoord, zCoord) == Block.waterStill.blockID)
+                return true;
+            if (worldObj.getBlockId(xCoord, yCoord, zCoord + 1) == Block.waterStill.blockID)
+                return true;
+            if (worldObj.getBlockId(xCoord, yCoord, zCoord - 1) == Block.waterStill.blockID)
+                return true;
+            return false;
+        } else
+            return false;
+    }
+
+    @Override
+    public void readFromNBT(NBTTagCompound par1NBTTagCompound) {
+        super.readFromNBT(par1NBTTagCompound);
+        NBTTagList nbttaglist = par1NBTTagCompound.getTagList("Items");
+        saltCondenserItemStacks = new ItemStack[this.getSizeInventory()];
+
+        for (int i = 0; i < nbttaglist.tagCount(); ++i) {
+            NBTTagCompound nbttagcompound1 = (NBTTagCompound) nbttaglist
+                    .tagAt(i);
+            byte b0 = nbttagcompound1.getByte("Slot");
+
+            if (b0 >= 0 && b0 < saltCondenserItemStacks.length) {
+                saltCondenserItemStacks[b0] = ItemStack
+                        .loadItemStackFromNBT(nbttagcompound1);
+            }
+        }
+
+        saltCondenserTimeRemaining = par1NBTTagCompound
+                .getInteger("TimeRemaining");
+        saltProgress = par1NBTTagCompound.getInteger("SaltProgress");
+        ceaseFunction = par1NBTTagCompound.getBoolean("CeaseFunction");
+
+        if (par1NBTTagCompound.hasKey("CustomName")) {
+            customName = par1NBTTagCompound.getString("CustomName");
+        }
+    }
+
+    @Override
+    public void writeToNBT(NBTTagCompound par1NBTTagCompound) {
+        super.writeToNBT(par1NBTTagCompound);
+        par1NBTTagCompound.setInteger("TimeRemaining",
+                saltCondenserTimeRemaining);
+        par1NBTTagCompound.setInteger("SaltProgress", saltProgress);
+        par1NBTTagCompound.setBoolean("CeaseFunction", ceaseFunction);
+        NBTTagList nbttaglist = new NBTTagList();
+
+        for (int i = 0; i < saltCondenserItemStacks.length; ++i) {
+            if (saltCondenserItemStacks[i] != null) {
+                NBTTagCompound nbttagcompound1 = new NBTTagCompound();
+                nbttagcompound1.setByte("Slot", (byte) i);
+                saltCondenserItemStacks[i].writeToNBT(nbttagcompound1);
+                nbttaglist.appendTag(nbttagcompound1);
+            }
+        }
+
+        par1NBTTagCompound.setTag("Items", nbttaglist);
+
+        if (this.isInvNameLocalized()) {
+            par1NBTTagCompound.setString("CustomName", customName);
+        }
+    }
+
+    @Override
+    public int getSizeInventory() {
+        return saltCondenserItemStacks.length;
+    }
+
+    @Override
+    public ItemStack getStackInSlot(int i) {
+        return saltCondenserItemStacks[i];
+    }
+
+    @Override
+    public ItemStack decrStackSize(int par1, int par2) {
+        if (saltCondenserItemStacks[par1] != null) {
+            ItemStack itemstack;
+
+            if (saltCondenserItemStacks[par1].stackSize <= par2) {
+                itemstack = saltCondenserItemStacks[par1];
+                saltCondenserItemStacks[par1] = null;
+                return itemstack;
+            } else {
+                itemstack = saltCondenserItemStacks[par1].splitStack(par2);
+
+                if (saltCondenserItemStacks[par1].stackSize == 0) {
+                    saltCondenserItemStacks[par1] = null;
+                }
+
+                return itemstack;
+            }
+        } else
+            return null;
+    }
+
+    @Override
+    public ItemStack getStackInSlotOnClosing(int par1) {
+        if (saltCondenserItemStacks[par1] != null) {
+            ItemStack itemstack = saltCondenserItemStacks[par1];
+            saltCondenserItemStacks[par1] = null;
+            return itemstack;
+        } else
+            return null;
+    }
+
+    @Override
+    public void setInventorySlotContents(int par1, ItemStack par2ItemStack) {
+        saltCondenserItemStacks[par1] = par2ItemStack;
+
+        if (par2ItemStack != null
+                && par2ItemStack.stackSize > this.getInventoryStackLimit()) {
+            par2ItemStack.stackSize = this.getInventoryStackLimit();
+        }
+    }
+
+    @Override
+    public String getInvName() {
+        return this.isInvNameLocalized() ? customName
+                : "container.saltCondenser";
+    }
+
+    @Override
+    public boolean isInvNameLocalized() {
+        return customName != null && customName.length() > 0;
+    }
+
+    @Override
+    public int getInventoryStackLimit() {
+        return 64;
+    }
+
+    @Override
+    public boolean isUseableByPlayer(EntityPlayer par1EntityPlayer) {
+        return worldObj.getBlockTileEntity(xCoord, yCoord, zCoord) != this ? false
+                : par1EntityPlayer.getDistanceSq(xCoord + 0.5D, yCoord + 0.5D,
+                        zCoord + 0.5D) <= 64.0D;
+    }
+
+    @Override
+    public void openChest() {
+    }
+
+    @Override
+    public void closeChest() {
+    }
+
+    @Override
+    public boolean isStackValidForSlot(int i, ItemStack itemstack) {
+        return false;
+    }
+
+}
