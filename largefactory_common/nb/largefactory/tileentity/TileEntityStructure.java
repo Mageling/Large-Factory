ec407de4b775211d463b0ddb28bd10f961b1e415
Mageling <nksadow@gmail.com>
2013-06-19 21:08:37 -0600

    NBT stuffs

--- largefactory_common/nb/largefactory/tileentity/TileEntityStructure.java
+++ largefactory_common/nb/largefactory/tileentity/TileEntityStructure.java
@@ -1,6 +1,9 @@
 package nb.largefactory.tileentity;
 
 import nb.largefactory.structure.StructureType;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
 
 public abstract class TileEntityStructure extends TileEntityLargeFactory {
 
@@ -121,4 +124,36 @@ public abstract class TileEntityStructure extends TileEntityLargeFactory {
         }
         return i;
     }
-}
+    
+    public void writeToNBT(NBTTagCompound par1NBTTagCompound) {
+        super.writeToNBT(par1NBTTagCompound);
+        par1NBTTagCompound.setString("Component Name", componentName);
+        par1NBTTagCompound.setBoolean("In Structure", inStructure);
+        NBTTagList nbttaglist = new NBTTagList();
+
+        for (int i = 0; i < controlBlockLocation.length; ++i) {
+                NBTTagCompound nbttagcompound1 = new NBTTagCompound();
+                nbttagcompound1.setInteger("Control Block Location:" + i, controlBlockLocation[i]);
+                nbttaglist.appendTag(nbttagcompound1);
+        }
+
+        par1NBTTagCompound.setTag("Control Block Location", nbttaglist);
+        
+    }
+    
+    public void readFromNBT(NBTTagCompound par1NBTTagCompound) {
+        super.readFromNBT(par1NBTTagCompound);
+        NBTTagList nbttaglist = par1NBTTagCompound.getTagList("Control Block Location");
+        controlBlockLocation = new int[nbttaglist.tagCount()];
+
+        for (int i = 0; i < nbttaglist.tagCount(); ++i) {
+            NBTTagCompound nbttagcompound1 = (NBTTagCompound) nbttaglist
+                    .tagAt(i);
+            controlBlockLocation[i] = nbttagcompound1.getInteger("Control Block Location:" + i);
+
+
+            }
+        componentName = par1NBTTagCompound.getString("Component Name");
+        inStructure = par1NBTTagCompound.getBoolean("In Structure");
+        }
+    }
