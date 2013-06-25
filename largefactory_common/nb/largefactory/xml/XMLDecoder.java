cfdafaa246a8e28848fbf3fbe6039828da2a9bcb
Benji Goldstein <benji.n.goldstein@gmail.com>
2013-06-25 09:16:13 -0600

    Worked on making it conver the NodeList to a HashMap

--- largefactory_common/nb/largefactory/xml/XMLDecoder.java
+++ largefactory_common/nb/largefactory/xml/XMLDecoder.java
@@ -1,6 +1,8 @@
 package nb.largefactory.xml;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -8,12 +10,15 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import nb.largefactory.structure.StructureType;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 public class XMLDecoder {
     static NodeList node;
-
+    static Map<String,Object> componentList;
+    
     public static void instantiate() {
+        componentList = new HashMap<String, Object>();
         try {
             File file = new File("test.xml");
             if (file.exists()) {
@@ -21,8 +26,12 @@ public class XMLDecoder {
                         .newInstance();
                 DocumentBuilder builder = fact.newDocumentBuilder();
                 Document doc = builder.parse("test.xml");
-                // Node node = doc.getDocumentElement();
                 node = doc.getElementsByTagName("*");
+                for (int i=0; i < node.getLength(); i++){
+                    if(node.item(i).getNodeName().equals("component")){
+                        componentList.put(node.item(i).getFirstChild().getTextContent(), createStructureObjectFromNode(node.item(i)));
+                    }
+                }
             }
         } catch (Exception e) {
         }
@@ -34,6 +43,10 @@ public class XMLDecoder {
                 return StructureType.stringToStructureType(node.item(i).getParentNode().getParentNode().getNodeName().replace("_components",""));
             }
         }
+        return null;
+    }
+    public static Object createStructureObjectFromNode(Node componentNode) {
+        // do some stuff
         return null;
     }
 }
