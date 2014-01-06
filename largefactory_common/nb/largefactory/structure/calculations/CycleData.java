51db3b92c26c205dc0b7cabdce7a04d3ac1591e3
Benji Goldstein <benji.n.goldstein@gmail.com>
2014-01-06 16:59:33 -0700

    Cyclework

--- largefactory_common/nb/largefactory/structure/calculations/CycleData.java
+++ largefactory_common/nb/largefactory/structure/calculations/CycleData.java
@@ -1,7 +1,8 @@
 package nb.largefactory.structure.calculations;
 
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
+import nb.largefactory.structure.calculations.StateEnum;
 
 /*
  * some high level stuff right here
@@ -10,23 +11,48 @@ import java.util.Map;
  * 
  */
 public class CycleData {
-    Float timeRequired = new Float(null);
-    
-    Map<String, Float> Blocks;
-    Map<String, Float> LiquidSlag1;
-    Map<String, Float> LiquidOre1;
-    Map<String, Float> GassSlag1;
-    Map<String, Float> GassOre1;
-    
-    public CycleData(HashMap<String, Float> input){
-        LiquidSlag1 = new HashMap<String, Float>();
-        LiquidOre1 = new HashMap<String, Float>();
-        GassSlag1 = new HashMap<String, Float>();
-        GassOre1 = new HashMap<String, Float>();
-        Blocks = input;
+    HashMap<String, HashMap<String, Float>> Resources;
+    ArrayList<Float> timeremaining;
+
+    public CycleData(StateEnum state, HashMap<String, Float> input, HashMap<String, Float> slag, Float time){
+        Resources = new HashMap<String, HashMap<String, Float>>();
+        timeremaining = new ArrayList<Float>();
+        timeremaining.set(0, time);
+        switch(state){
+            case Gas: Resources.put("MainGas",input);
+                Resources.put("MainGasSlag",slag);
+                break;
+            case Liquid: Resources.put("MainLiquid",input);
+                Resources.put("MainLiquidSlag",slag);
+                break;
+            case Solid: Resources.put("MainSolid",input);
+                Resources.put("MainSolidSlag",slag);
+                break;
+            default:    Resources.put("UNKNOWNSTATE", input);
+                Resources.put("UNKNOWNSTATESlag", input);
+                break;
+        }
     }
     
-    public HashMap GetBlocks(){
-        return (HashMap) Blocks;
+    public HashMap<String, Float> GetRecInfo(String name){
+        return Resources.get(name);
+    }
+    
+    public void SetRecInfo(String name, HashMap<String, Float> value){
+        Resources.put(name, value);
+    }
+    
+    public HashMap<String, HashMap<String, Float>> GetFullInfo(){
+        return Resources;
+    }
+        
+    public Float GetTimeRequired(){
+        return timeremaining;
+    }
+    
+    public void AddTimeRequired(Float time, int number){
+        timeremaining.set(number, time);
     }
 }
+    
+
---
+++ largefactory_common/nb/largefactory/structure/calculations/StateEnum.java
@@ -0,0 +1,5 @@
+package nb.largefactory.structure.calculations;
+
+public enum StateEnum {
+    Solid, Liquid, Gas
+}
