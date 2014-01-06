1c076636ec6ad443ba19d40b9c2b506867c75c36
Benji Goldstein <benji.n.goldstein@gmail.com>
2014-01-06 14:02:16 -0700

    Testing

--- largefactory_common/nb/largefactory/lib/Files.java
+++ largefactory_common/nb/largefactory/lib/Files.java
@@ -2,5 +2,6 @@ package nb.largefactory.lib;
 
 public class Files {
     public static final String XML_FILE_LOCATION = "/mod/largefactory/files/LargeFactory.xml";
-    public static final String DEFAULT_TEXTURE_FILE_LOCATION = "/mod/largefactory/";
+    public static final String DEFAULT_TEXTURE_FILE_LOCATION = "";
+
 }
--- largefactory_common/nb/largefactory/structure/calculations/CycleData.java
+++ largefactory_common/nb/largefactory/structure/calculations/CycleData.java
@@ -1,5 +1,27 @@
 package nb.largefactory.structure.calculations;
 
-public class CycleData {
+import java.util.HashMap;
+import java.util.Map;
 
+/*
+ * some high level stuff right here
+ * Cycle data needs to contain everything
+ * It starts with a hashmap of imported blocks
+ * 
+ */
+public class CycleData {
+    Float timeRequired = new Float(null);
+    
+    public CycleData(HashMap<String, Float> input){
+        Map<String, Float> Blocks = new HashMap<String, Float>();
+        Map<String, Float> LiquidSlag1 = new HashMap<String, Float>();
+        Map<String, Float> LiquidOre1 = new HashMap<String, Float>();
+        Map<String, Float> GassSlag1 = new HashMap<String, Float>();
+        Map<String, Float> GassOre1 = new HashMap<String, Float>();
+        Blocks = input;
+    }
+    
+    public HashMap GetBlocks(){
+        return Blocks;
+    }
 }
